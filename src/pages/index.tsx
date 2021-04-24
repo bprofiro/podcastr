c26836d816ec2f8a5e7f5c620ab98937756b7ec8
Brenda <brendaprofiro@gmail.com>
2021-04-24 19:17:32 -0300

    chore: Start home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,36 @@
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home({ episodes }: HomeProps) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(episodes)}</p>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('/episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
+  return {
+    props: { episodes: data },
+    revalidate: 60 * 60 * 8,
+  }
+}
